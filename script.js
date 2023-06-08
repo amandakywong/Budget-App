d5d39f82b3b77623e4414c1b592feb7fa14b1935
amandakywong <amandakywong@outlook.com>
2023-06-08 15:27:05 +1000

    Dynamic table

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const tableExpense = document.getElementById("dynamicTableSpend");
+const tableSave = document.getElementById("dynamicTableSav");
 
 const logExpenseButton = document.getElementById("logexpensebutton");
 const logSavingButton = document.getElementById("logsavingbutton");
@@ -8,160 +10,133 @@ const dateExpense = document.getElementById("inputdatespent");
 const dollarSave = document.getElementById("inputdollarsaved");
 const dateSave = document.getElementById("inputdatesaved");
 
-const tableExpense = document.getElementById("dynamicTableSpend");
-const tableSave = document.getElementById("dynamicTableSav");
+const alertExpense = document.getElementById("alertSpend");
+const alertSave = document.getElementById("alertSav");
 
-let expenseData = {}
-let saveData = {}
+let expenseData = {};
+let saveData = {};
 
-logExpenseButton.addEventListener("click", expenseInData)
-logSavingButton.addEventListener("click", saveInData)
+logExpenseButton.addEventListener("click", expenseInData);
+logSavingButton.addEventListener("click", saveInData);
 
 function round(number) {
   let roundedNum = Math.round(number / 0.05) * 0.05;
-  return roundedNum.toFixed(2)
+  return roundedNum.toFixed(2);
+}
+function formatDate(date) {
+  const [year, month, day] = date.split("-");
+  return `${day}/${month}/${year}`;
+}
+function sortObjectByDate(obj) {
+  const sortedArray = Object.entries(obj).sort(([keyA], [keyB]) => {
+    const dateA = new Date(keyA);
+    const dateB = new Date(keyB);
+    return dateA - dateB;
+  });
+  const sortedObject = Object.fromEntries(sortedArray);
+  return sortedObject;
 }
 
-function expenseInData(){
-  const finalDoll = (round(dollarExpense.value))
-  const finalDate = dateExpense.value
-  if (Object.keys(expenseData).length === 0){
-    expenseData = {[finalDate]: finalDoll};
+function expenseInData() {
+  const finalDoll = round(dollarExpense.value);
+  const finalDate = dateExpense.value;
+  if (dollarExpense.value === "" && dateExpense.value === "") {
+    alertExpense.innerHTML = "*Please fill in both fields*";
+    return;
+  }
+  if (dollarExpense.value === "") {
+    alertExpense.innerHTML = "*Please enter amount spent*";
+    return;
+  }
+  if (dateExpense.value === "") {
+    alertExpense.innerHTML = "*Please enter date spent*";
+    return;
+  }
+  if (Object.keys(expenseData).length === 0) {
+    expenseData = { [finalDate]: finalDoll };
   } else {
     expenseData[finalDate] = finalDoll;
   }
-  displayExpense()
+  expenseData = sortObjectByDate(expenseData);
+  displayExpense();
 }
 
-function displayExpense(){
+function displayExpense() {
   while (tableExpense.rows.length > 1) {
     tableExpense.deleteRow(1);
   }
-  
-  Object.entries(expenseData).forEach(([key, value]) => {
+
+  //alternative loop code
+  // Object.entries(expenseData).forEach(([key, value]) => {
+  //   const row = tableExpense.insertRow();
+  //   const keyCell = row.insertCell();
+  //   const valueCell = row.insertCell();
+
+  //   keyCell.textContent = formatDate(key);
+  //   valueCell.textContent = value;
+  // });
+
+  const key = Object.keys(expenseData);
+  const value = Object.values(expenseData);
+ 
+  for (let i = 0; i <key.length; i++) {
     const row = tableExpense.insertRow();
     const keyCell = row.insertCell();
     const valueCell = row.insertCell();
-  
-    keyCell.textContent = key;
-    valueCell.textContent = value;
-  });
+
+    keyCell.textContent = key[i];
+    valueCell.textContent = value[i];
+  }
+  clearInput();
 }
 
-function saveInData(){
-
+function saveInData() {
+  const finalDoll = round(dollarSave.value);
+  const finalDate = dateSave.value;
+  if (dollarSave.value === "" && dateSave.value === "") {
+    alertSave.innerHTML = "*Please fill in both fields*";
+    return;
+  }
+  if (dollarSave.value === "") {
+    alertSave.innerHTML = "*Please enter amount saved*";
+    return;
+  }
+  if (dateSave.value === "") {
+    alertSave.innerHTML = "*Please enter date saved*";
+    return;
+  }
+  if (Object.keys(saveData).length === 0) {
+    saveData = { [finalDate]: finalDoll };
+  } else {
+    saveData[finalDate] = finalDoll;
+  }
+  saveData = sortObjectByDate(saveData);
+  displaySave();
 }
 
-function displaySave(){
+function displaySave() {
+  while (tableSave.rows.length > 1) {
+    tableSave.deleteRow(1);
+  }
+  const key = Object.keys(saveData);
+  const value = Object.values(saveData);
+ 
+  for (let i = 0; i <key.length; i++) {
+    const row = tableSave.insertRow();
+    const keyCell = row.insertCell();
+    const valueCell = row.insertCell();
 
+    keyCell.textContent = key[i];
+    valueCell.textContent = value[i];
+  }
+  clearInput();
 }
 
-function clearData() {
-
+function clearInput() {
+  dollarExpense.value = "";
+  dateExpense.value = "";
+  dollarSave.value = "";
+  dateSave.value = "";
+  alertExpense.innerHTML = "";
+  alertSave.innerHTML = "";
 }
-
-
-/*
-logExpenseButton.addEventListener("mouseup", function () {
-  let existingDollarSpent = localStorage.getItem("dollarSpent");
-  existingDollarSpent = existingDollarSpent ? JSON.parse(existingDollarSpent) : [];
-  existingDollarSpent.push(dollarSpent);
-  localStorage.setItem("dollarSpent", JSON.stringify(existingDollarSpent));
-  let dollarSpent = "";
-
-  let existingDateSpent = localStorage.getItem("dateSpent");
-  existingDateSpent = existingDateSpent ? JSON.parse(existingDateSpent) : [];
-  existingDateSpent.push(dateSpent);
-  localStorage.setItem("dateSpent", JSON.stringify(existingDateSpent));
-  let dateSpent = "";
-
-  displayDataSpent();
-})
-
-function displayDataSpent() {
-  const storedDollarData = localStorage.getItem("dollarSpent");
-  const dollarData = storedDollarData ? JSON.parse(storedDollarData) : [];
-  const storedDateData = localStorage.getItem("dateSpent");
-  const dateData = storedDateData ? JSON.parse(storedDateData) : [];
-
-  const table = document.getElementById("dynamicTableSpend");
-  while (table.rows.length > 1) {
-    table.deleteRow(1);
-  }
-  for (let i = 0; i < Math.max(dollarData.length, dateData.length); i++) {
-    const row = table.insertRow();
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
-
-    if (i < dateData.length) {
-      cell1.textContent = dateData[i];
-    } else {
-      cell1.textContent = "";
-    }
-
-    if (i < dollarData.length) {
-      cell2.textContent = dollarData[i];
-    } else {
-      cell2.textContent = "";
-    }
-  }
-  }
-
-logSavingButton.addEventListener("mouseup", function () {
-
-  let existingDollarSaved = localStorage.getItem("dollarSav");
-  existingDollarSaved = existingDollarSaved ? JSON.parse(existingDollarSaved) : [];
-  existingDollarSaved.push(dollarSaved);
-  localStorage.setItem("dollarSav", JSON.stringify(existingDollarSaved));
-  let dollarSaved = "";
-
-
-  let existingDateSaved = localStorage.getItem("dateSav");
-  existingDateSaved = existingDateSaved ? JSON.parse(existingDateSaved) : [];
-  existingDateSaved.push(dateSaved);
-  localStorage.setItem("dateSav", JSON.stringify(existingDateSaved));
-  let dateSaved = "";
-
-  displayDataSav();
-
-  if (dollarSpent=== NaN){
-    document.getElementById("alertSpend").innerHTML = "Enter number"
-  }
-})
-
-
-function displayDataSav() {
-  const storedDollarData = localStorage.getItem("dollarSav");
-  const dollarData = storedDollarData ? JSON.parse(storedDollarData) : [];
-  const storedDateData = localStorage.getItem("dateSav");
-  const dateData = storedDateData ? JSON.parse(storedDateData) : [];
-
-  const table = document.getElementById("dynamicTableSav");
-  while (table.rows.length > 1) {
-    table.deleteRow(1);
-  }
-  for (let i = 0; i < Math.max(dollarData.length, dateData.length); i++) {
-    const row = table.insertRow();
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
-
-    if (i < dateData.length) {
-      cell1.textContent = dateData[i];
-    } else {
-      cell1.textContent = "";
-    }
-
-    if (i < dollarData.length) {
-      cell2.textContent = dollarData[i];
-    } else {
-      cell2.textContent = "";
-    }
-  }
-  }
-*/
-
-
-/*
-  * add required date and dollar
-  * restrictions on number dollar
-*/
